7117021866468ce0336562fb5e86fab187269b9e
Action <572333932@qq.com>
2014-08-08 11:40:21 +0800

    分类模块的数据库设计

---
+++ docs/category_init_01_create.sql
@@ -0,0 +1,36 @@
+-- 建表
+-- Table: bc_category
+
+--DROP TABLE bc_category;
+
+CREATE TABLE bc_category
+(
+  id integer NOT NULL,
+  pid integer NOT NULL DEFAULT 0,
+  status_ integer NOT NULL DEFAULT 0,
+  code character varying(100) NOT NULL CHECK(code not like '%/%') ,
+  name_ character varying(255) NOT NULL,
+  sn character varying(100),
+  modifier_id integer NOT NULL,
+  modified_date timestamp without time zone NOT NULL,
+  CONSTRAINT pk_bc_category PRIMARY KEY (id),
+  CONSTRAINT bsfk_category_modified FOREIGN KEY (modifier_id)
+      REFERENCES bc_identity_actor_history (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  UNIQUE(pid, code)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE bc_category
+  OWNER TO bcsystem;
+GRANT ALL ON TABLE bc_category TO bcsystem;
+COMMENT ON TABLE bc_category
+  IS '分类';
+COMMENT ON COLUMN bc_category.pid IS '父类别ID';
+COMMENT ON COLUMN bc_category.status_ IS '状态，0-正常、1-禁用';
+COMMENT ON COLUMN bc_category.code IS '编码';
+COMMENT ON COLUMN bc_category.name_ IS '名称';
+COMMENT ON COLUMN bc_category.sn IS '同级节点间的顺序号';
+COMMENT ON COLUMN bc_category.modifier_id IS '最后修改人ID';
+COMMENT ON COLUMN bc_category.modified_date IS '最后修改时间';
