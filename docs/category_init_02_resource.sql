a78a3143bfce274c69cfa9d17cf99e51fee667e9
LeeDane <825711424@qq.com>
2014-08-21 16:12:52 +0800

    优化数据库设计

--- docs/category_init_02_resource.sql
+++ docs/category_init_02_resource.sql
@@ -94,3 +94,53 @@ insert into BC_IDENTITY_ACTOR_RELATION (TYPE_,MASTER_ID,FOLLOWER_ID)
     where am.CODE='CategoryManageGroup' 
 	and af.CODE in ('dragon') -- 用户帐号
 	and not exists (select 0 from BC_IDENTITY_ACTOR_RELATION r where r.TYPE_=0 and r.MASTER_ID=am.id and r.FOLLOWER_ID=af.id);
+	
+-- 插入模板
+
+-- 平台/分类管理
+insert into bc_template (id,order_,code,content,file_date,author_id,modifier_id,modified_date,subject,
+	path,inner_,desc_,status_,version_,category,type_id,size_,formatted,uid_)
+	select nextval('core_sequence'),'5004','BC-CATEGORY',null,now(),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),now(),
+		'分类管理','bc/categoryView.v1.sql.ftl',false,'分类管理模块SQL查询语句模板',0,'1.0','平台/分类管理',
+		(select id from bc_template_type where code='freemarker'),
+		0,false,'Template.category.1'
+	from BC_DUAL 
+	where not exists (select 0 from bc_template where code='BC-CATEGORY');
+
+-- 平台/分类管理总数
+insert into bc_template (id,order_,code,content,file_date,author_id,modifier_id,modified_date,subject,
+	path,inner_,desc_,status_,version_,category,type_id,size_,formatted,uid_)
+	select nextval('core_sequence'),'5005','BC-CATEGORY-COUNT',null,now(),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),now(),
+		'分类管理','bc/categoryView.v1.countSql.ftl',false,'分类管理模块总行数SQL查询语句模板',0,'1.0','平台/分类管理',
+		(select id from bc_template_type where code='freemarker'),
+		0,false,'Template.category.2'
+	from BC_DUAL 
+	where not exists (select 0 from bc_template where code='BC-CATEGORY-COUNT');
+	
+-- 平台/分类管理/选择分类
+insert into bc_template (id,order_,code,content,file_date,author_id,modifier_id,modified_date,subject,
+	path,inner_,desc_,status_,version_,category,type_id,size_,formatted,uid_)
+	select nextval('core_sequence'),'5006','BC-SELECT-CATEGORY',null,now(),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),now(),
+		'选择分类','bc/categorySelectView.v1.sql.ftl',false,'选择分类视图SQL主模板',0,'1.0','平台/分类管理/选择分类',
+		(select id from bc_template_type where code='freemarker'),
+		0,false,'Template.category.3'
+	from BC_DUAL 
+	where not exists (select 0 from bc_template where code='BC-SELECT-CATEGORY');
+	
+-- 平台/分类管理/选择分类计数
+insert into bc_template (id,order_,code,content,file_date,author_id,modifier_id,modified_date,subject,
+	path,inner_,desc_,status_,version_,category,type_id,size_,formatted,uid_)
+	select nextval('core_sequence'),'5007','BC-SELECT-CATEGORY-COUNT',null,now(),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),
+		(select id from bc_identity_actor_history where actor_code='admin' and current=true),now(),
+		'选择分类计数','bc/categorySelectView.v1.count.sql.ftl',false,'选择分类视图SQL计数模板',0,'1.0','平台/分类管理/选择分类计数',
+		(select id from bc_template_type where code='freemarker'),
+		0,false,'Template.category.4'
+	from BC_DUAL 
+	where not exists (select 0 from bc_template where code='BC-SELECT-CATEGORY-COUNT');
